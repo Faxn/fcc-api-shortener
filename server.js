edfcd6e622c02db18f5ac09c08ecacb834f25c1f
Blake Curry <blake.curry@gmx.us>
2017-01-10 16:38:13 -0500

    Added ability to configure database url with environment variables instead of hard coded localhost.

--- server.js
+++ server.js
@@ -3,24 +3,39 @@ var app = express()
 
 app.set('view engine', 'pug');
 
-service_url = "halberd:8080"
+service_url = process.env.SERVICE_URL
 
 //var mongo = require('tingodb')()
 //var db = new mongo.Db(process.cwd()+'/data', {})
 
 var mongo = require('mongodb')
-var db = new mongo.Db("test", mongo.Server("localhost", 27017) )
+var MongoClient=mongo.MongoClient
+
+db_url = process.env.DB_URL
+
+//making a false db object to mimic the deprecated syntax of the Node.js mongodb driver.
+//this allows for the code to stay more compatible with tingodb.
+var db = {};
+db.open = function(callback){
+    MongoClient.connect(db_url, function(err, db){
+        callback(err, db);
+    });
+}
+
 
 app.get('/', function(req, res, next){
     res.render('help', {url:service_url});
 })
+
+
+
 
 app.all('/new/*', function(req, res, next){
     id = Math.floor(Math.random() * 10000)
     
     link = req.path.substr(5)
     
-    if (link.match(/(https?:).*/) ){
+    if (link.match(/(https?:\/\/).*/) ){
         db.open(function(err, db){
             if(err) {
                 console.log(err)
